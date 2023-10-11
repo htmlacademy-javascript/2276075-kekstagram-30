b80a0571d0fa9f8e72a0c25ef9f6f1d95e96bc17
андрей <dron.wak@mail.ru>
2023-10-11 14:00:04 +0200

    добавил проверку длины строки

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+function checksLength (string, number) {
+  return string.length <= number;
+}
+
+console.log(checksLength('проверяемая строка', 20));
+console.log(checksLength('проверяемая строка', 18));
+console.log(checksLength('проверяемая строка', 10));

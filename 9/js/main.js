4c2a631934118e3bb3dfb3e181891192c836403f
Keks <keks@htmlacademy.ru>
2023-11-02 16:35:34 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,4 @@
+import { getRandomInteger, getRandomElement, createIdGenerator } from "./util.js";
+import { createObject } from "./data.js";
+import { renderPicture } from "./create-pictures.js";
+console.log(createObject(25));
